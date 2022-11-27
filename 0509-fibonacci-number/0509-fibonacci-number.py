5f7c728b3a67a4c745b1b1a64881b492e73604ef
chiendvhust <67858346+chiendvhust@users.noreply.github.com>
2022-11-27 23:39:59 +0700

    Time: 60 ms (49.81%), Space: 13.8 MB (96.21%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -2,7 +2,7 @@ class Solution:
     memo = {}
     def fib(self, n: int) -> int:
         if n <= 1: return n
-        if n in Solution.memo: return Solution.memo[n]
+        if n in self.memo: return self.memo[n]
         
-        Solution.memo[n] = self.fib(n-1) + self.fib(n-2)
-        return Solution.memo[n]
+        self.memo[n] = self.fib(n-1) + self.fib(n-2)
+        return self.memo[n]
