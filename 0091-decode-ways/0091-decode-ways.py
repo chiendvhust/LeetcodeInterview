5ef84a2ad466512d5451e90d7a96f9c43db0309e
chiendvhust <67858346+chiendvhust@users.noreply.github.com>
2022-11-28 11:39:22 +0700

    Time: 50 ms (73.09%), Space: 14.1 MB (21.55%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -10,7 +10,7 @@ class Solution:
             res = 0
             if s[i] != '0':
                 res += dp(i + 1)
-            if i + 1 < n and (s[i] == '1' or s[i] == '2' and s[i + 1] <= '6'):
+            if i + 1 < n and (s[i] == '1' or (s[i] == '2' and s[i + 1] <= '6')):
                 res += dp(i + 2)
             return res
         return dp(0)
